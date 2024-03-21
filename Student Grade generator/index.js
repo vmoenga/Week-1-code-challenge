588547d74397674ec0ab37bd10f8a0a0162b619d
Victor <vmoenga36@gmail.com>
2024-03-21 17:11:15 +0300

    grade generator

--- Student Grade generator/index.js
+++ Student Grade generator/index.js
@@ -1,6 +1,25 @@
 const numericGrade = document.getElementById('numericGrade');
+const gradeDisplay = document.getElementById('gradeDisplay'); 
 
 function convert() {
-    const numeric = numericGrade.value;
-    console.log('we are here', numeric);
+    const grade = parseInt(numericGrade.value); 
+    
+    let myGrade = "";
+    if (grade >= 80 && grade <= 100) {
+        myGrade = "A"; 
+    } 
+    else if (grade >= 60 && grade <= 79) {
+        myGrade = "B"; 
+    } 
+    else if (grade >= 50 && grade <= 59) {
+         myGrade = "C"; 
+    } 
+    else if (grade >= 40 && grade <= 49) {
+        myGrade = "D"; 
+    }
+    else if (grade < 40) { 
+        myGrade = "E"; 
+    }
+    console.log(myGrade); 
+    gradeDisplay.innerText = `Your letter grade is ${myGrade}`; 
 }
